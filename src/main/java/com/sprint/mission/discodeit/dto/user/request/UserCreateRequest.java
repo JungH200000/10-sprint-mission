2735555f329b6a365d3fcbebf510bd8d24c5e473
JungH200000 <jhpaxk0604@gmail.com>
2026-02-18 23:10:34 +0900

    refactor: `birthday`를 필수 입력에서 제외

--- src/main/java/com/sprint/mission/discodeit/dto/user/request/UserCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/user/request/UserCreateRequest.java
@@ -18,7 +18,7 @@ public record UserCreateRequest(
         @Pattern(regexp = "^\\S+$", message = "password는 공백이 허용되지 않습니다.")
         String password,
 
-        @NotBlank(message = "birthday가 입력되지 않았습니다.")
+//        @NotBlank(message = "birthday가 입력되지 않았습니다.")
         @Pattern(regexp = "^\\d{8}$", message = "birthday는 YYYYMMDD 형식이어야 합니다.")
         String birthday
 ) {
