b244d8414d6e8fa0173325b921c7ae0a27b73ecb
JungH200000 <jhpaxk0604@gmail.com>
2026-02-18 17:33:18 +0900

    refactor: 구조 수정

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -47,8 +47,9 @@ public class UserController {
             @ApiResponse(responseCode = "201", description = "User가 성공적으로 생성됨", content = @Content(schema = @Schema(implementation = User.class))),
             @ApiResponse(responseCode = "400", description = "같은 email이나 username을 사용하는 User가 이미 존재함", content = @Content(examples = @ExampleObject(value = "User with newEmail {newEmail} already exists")))
     })
-    public ResponseEntity<UserDto> create(@RequestPart @Valid UserCreateRequest userCreateRequest,
-                                    @RequestPart(required = false) @Schema(description = "User 프로필 이미지") MultipartFile profile) {
+    public ResponseEntity<UserDto> create(
+            @RequestPart @Valid UserCreateRequest userCreateRequest,
+            @RequestPart(required = false) @Schema(description = "User 프로필 이미지") MultipartFile profile) {
         User user = userService.createUser(userCreateRequest, profile);
         UserDto result = createUserResponse(user);
         return ResponseEntity.status(201).body(result);
