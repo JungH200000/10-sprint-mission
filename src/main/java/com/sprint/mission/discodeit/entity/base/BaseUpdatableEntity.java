afc575b66adfc4df0b981db069f4c36f597e1042
JungH200000 <jhpaxk0604@gmail.com>
2026-03-04 10:39:19 +0900

    feat: `updatedAt` 관련 `BaseUpdatablEntity` 생성

---
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
@@ -0,0 +1,25 @@
+package com.sprint.mission.discodeit.entity.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseUpdatableEntity extends BaseEntity {
+
+    @LastModifiedDate
+//    @Column(name = "")
+    protected Instant updatedAt; // 객체 수정 시간
+
+    // update 시간 메소드
+    public void updateTime() {
+        this.updatedAt = Instant.now();
+    }
+}
