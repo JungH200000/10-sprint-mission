73c1be4670bc72bd174ee67d042e74bfea8e6aa9
JungH200000 <jhpaxk0604@gmail.com>
2026-03-06 15:07:51 +0900

    feat: auditing 기능 동작을 안내하는 `@EnableJpaAuditing` 애너테이션을 메인 애플리케이션 클래스에 추가

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -4,13 +4,12 @@ import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DiscodeitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DiscodeitApplication.class, args);
-		UserRepository userRepository;
-		UserStatusRepository userStatusRepository;
-
 	}
 }
