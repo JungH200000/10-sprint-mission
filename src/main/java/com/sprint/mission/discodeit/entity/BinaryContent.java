c5018f427133ad7090092365b06eca242456118e
JungH200000 <jhpaxk0604@gmail.com>
2026-03-06 18:47:24 +0900

    refactor: 바이너리 데이터 필드인 `bytes` 제외

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -4,7 +4,6 @@ import com.sprint.mission.discodeit.entity.base.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 /**
  * 이미지, 파일 등 바이너리 데이터를 표현하는 도메인 모델로,
@@ -26,19 +25,10 @@ public class BinaryContent extends BaseEntity {
     @Column(nullable = false)
     private String contentType;
 
-    @Column(nullable = false)
-    private byte[] bytes;
-
     // 생성자
-    public BinaryContent(String fileName, String contentType, byte[] bytes, Long size) {
+    public BinaryContent(String fileName, String contentType, Long size) {
         this.fileName = fileName;
         this.size = size;
         this.contentType = contentType;
-        this.bytes = bytes.clone();
-    }
-
-    // getter
-    public byte[] getBytes() {
-        return bytes.clone();
     }
 }
