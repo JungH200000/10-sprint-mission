89f700aad89323fff8b65c5afe1a7e901b158d92
JungH200000 <jhpaxk0604@gmail.com>
2026-03-06 15:10:05 +0900

    refactor: `persist`되지 않게 하는 UUID 랜덤 생성 로직을 가진 생성자를 삭제하고, `@NoArgsConstructor` 애너테이션 추가

--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.entity.base;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -10,21 +11,15 @@ import java.util.UUID;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
 @Getter
 public abstract class BaseEntity  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    protected final UUID id; // 객체 식별을 위한 id
+    protected UUID id; // 객체 식별을 위한 id
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    protected final Instant createdAt; // 객체 생성 시간: "2026-01-28T00:52:05.985737500Z"
-
-    protected BaseEntity() {
-        // id 초기화
-        this.id = UUID.randomUUID();
-        // 시간 초기화
-        this.createdAt = Instant.now();
-    }
+    protected Instant createdAt; // 객체 생성 시간: "2026-01-28T00:52:05.985737500Z"
 }
