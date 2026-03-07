e75320031af65b7807b7ec487e9fd2ce37387d87
JungH200000 <jhpaxk0604@gmail.com>
2026-03-07 14:54:15 +0900

    feat: 바이너리 데이터 다운로드 메서드 `download` 구현

--- src/main/java/com/sprint/mission/discodeit/storage/local/LocalBinaryContentStorage.java
+++ src/main/java/com/sprint/mission/discodeit/storage/local/LocalBinaryContentStorage.java
@@ -8,13 +8,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.UUID;
@@ -72,6 +70,7 @@ public class LocalBinaryContentStorage implements BinaryContentStorage {
         Path path = resolvePath(binaryContentId);
 
         try {
+            // 파일을 읽을 수 있는 통로를 연다.
             return Files.newInputStream(path);
         } catch(IOException e) {
             throw new IllegalStateException("조회 실패", e);
@@ -80,9 +79,34 @@ public class LocalBinaryContentStorage implements BinaryContentStorage {
 
     @Override
     public ResponseEntity<Resource> download(BinaryContentDto binaryContentDto) {
-        InputStream inputStream = get(binaryContentDto.id());
-        InputStreamResource resource = new InputStreamResource(inputStream);
+        // Resource 타입 : Spring에서 파일, 스트림 같은 데이터 자원을 표현하는 타입
 
-        return null;
+        // InputStream :  바이트 데이터를 읽기 위한 통로
+        InputStream inputStream = get(binaryContentDto.id());
+
+        // InputStreamResource : Resource의 구현체 중 하나로
+        // 이미 얻어온 InputStream을 Spring이 처리하기 쉬운 Resource 형태로 감싼다.
+        // Spring은 ResponseEntity의 body로 파일/스트림 같은 자원을 보낼 때 Resource 타입을 잘 처리한다.
+        Resource resource = new InputStreamResource(inputStream);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                // Content-Type 헤더 설정
+                // MediaType.parseMediaType(type) : type(`image/png` 등)을 Spring의 MediaType 객체로 변경
+                .contentType(MediaType.parseMediaType(binaryContentDto.contentType()))
+                // Content-Length 헤더 설정
+                // 응답 본문 크기가 몇 바이트인지 알려주는 헤더
+                .contentLength(binaryContentDto.size())
+                .header(
+                        // Content-Disposition 헤더 설정
+                        // 브라우저에서 이 응답을 첨부파일처럼 다운로드하라고 알려줌
+                        HttpHeaders.CONTENT_DISPOSITION,
+                        // ContentDisposition 객체 생성
+                        ContentDisposition.attachment() // ContentDisposition 빌더
+                                // attachment()` : 브라우저가 응답을 화면에 바로 표시하기보다 다운로드 대상으로 처리하도록 유도
+                                .filename(binaryContentDto.fileName()) // 파일명 설정
+                                .build() // 객체 완성
+                                .toString() // HTTP 헤더 값은 문자열이어야 하기 때문에 문자열로 변환
+                )
+                .body(resource);
     }
 }
