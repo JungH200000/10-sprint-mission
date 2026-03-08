e06d690ed5c7e1420e59ca6b8446790a94d84015
JungH200000 <jhpaxk0604@gmail.com>
2026-03-08 16:46:36 +0900

    feat: 페이징을 위한 `PageResponse` DTO와 `PageResponseMapper` 구현
    - `PageResponse` : 일관된 페이지네이션 응답 DTO
    - `PageResponseMapper` : Slice나 Page 객체로 DTO 생성하는 Mapper

---
+++ src/main/java/com/sprint/mission/discodeit/dto/response/PageResponse.java
@@ -0,0 +1,12 @@
+package com.sprint.mission.discodeit.dto.response;
+
+import java.util.List;
+
+public record PageResponse<T>(
+        List<T> content, // 실제 데이터
+        int number, // 페이지 번호
+        int size, // 페이지 크기
+        boolean hasNext,
+        Long totalElements // T 데이터의 총 갯수(null일 수 있음)
+) {
+}
