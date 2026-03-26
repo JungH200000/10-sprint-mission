940fc1a9c255d2d99123e33258f90af054ac5e94
JungH200000 <jhpaxk0604@gmail.com>
2026-03-26 15:24:39 +0900

    feat: 전역 예외 처리 `GlobalExceptionHandler` 클래스의 각 예외 로직에 `log` 추가
    - 추후 커스텀 예외로 수정 예정

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -12,24 +13,29 @@ import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity handleException(IllegalArgumentException e) {
+        log.warn("[EXCEPTION] 잘못된 요청: status={}, message={}", HttpStatus.BAD_REQUEST.value(),e.getMessage(), e);
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity handleException(NoSuchElementException e) {
+        log.warn("[EXCEPTION] 해당 데이터를 찾을 수 없음: status={}, message={}", HttpStatus.NOT_FOUND.value(), e.getMessage(), e);
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.getReasonPhrase(), e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity handleException(IllegalStateException e) {
+        log.error("[EXCEPTION] 처리 중 오류 발생: status={}, message={}", HttpStatus.CONFLICT.value(), e.getMessage(), e);
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT.getReasonPhrase(), e.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity handleException(MethodArgumentNotValidException e) {
+        log.warn("[EXCEPTION] Bean Validation 예외 발생: status={}, message={}", HttpStatus.BAD_REQUEST.value(), e.getMessage(), e);
         String ErrorMessage = e.getBindingResult().getFieldErrors().stream()
                 .map(fe -> fe.getField() + ": " + fe.getDefaultMessage())
                 .collect(Collectors.joining(", "));
@@ -38,6 +44,7 @@ public class GlobalExceptionHandler {
     }
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handleException(HttpMessageNotReadableException e) {
+        log.warn("[EXCEPTION] 적합하지 않은 HTTP Request Body: status={}, message={}", HttpStatus.BAD_REQUEST.value(), e.getMessage(), e);
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
