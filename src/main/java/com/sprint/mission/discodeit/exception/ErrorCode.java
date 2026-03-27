8f0c19c3e62516474d5c8699cbdcac06017b2c39
JungH200000 <jhpaxk0604@gmail.com>
2026-03-27 09:30:28 +0900

    refactor: 요구사항에 따라 `ExceptionCode` Enum 파일의 이름을 `ErrorCode`로 변경

--- src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
@@ -3,13 +3,13 @@ package com.sprint.mission.discodeit.exception;
 import lombok.Getter;
 
 @Getter
-public enum ExceptionCode {
+public enum ErrorCode {
     MEMBER_NOT_FOUND(404, "Member Not Found");
 
     private int status;
     private String message;
 
-    ExceptionCode(int status, String message) {
+    ErrorCode(int status, String message) {
         this.status = status;
         this.message = message;
     }
