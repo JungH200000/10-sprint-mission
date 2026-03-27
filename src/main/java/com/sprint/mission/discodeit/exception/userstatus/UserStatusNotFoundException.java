4b651e3fab93eef51620475ff8712f129be0d750
JungH200000 <jhpaxk0604@gmail.com>
2026-03-27 15:46:48 +0900

    feat: `UserStatusException`을 상속 받는 `UserStatusNotFoundException` 정의

--- src/main/java/com/sprint/mission/discodeit/exception/userstatus/UserStatusNotFoundException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/userstatus/UserStatusNotFoundException.java
@@ -1,4 +1,12 @@
 package com.sprint.mission.discodeit.exception.userstatus;
 
-public class UserStatusNotFoundException {
+import com.sprint.mission.discodeit.exception.ErrorCode;
+
+import java.util.UUID;
+
+public class UserStatusNotFoundException extends UserStatusException {
+
+    public UserStatusNotFoundException(String key, UUID value) {
+        super(ErrorCode.USER_STATUS_NOT_FOUND, key, value);
+    }
 }
