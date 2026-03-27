e4ae29cf41d5a5a0b35d296bea99353b3247160b
JungH200000 <jhpaxk0604@gmail.com>
2026-03-27 17:00:30 +0900

    feat: 기존 `ErrorResponse`에 `DiscodeitException`에서 정의한 필드 추가

--- src/main/java/com/sprint/mission/discodeit/exception/ErrorResponse.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ErrorResponse.java
@@ -3,9 +3,16 @@ package com.sprint.mission.discodeit.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.Instant;
+import java.util.Map;
+
 @Getter
 @AllArgsConstructor
 public class ErrorResponse {
+    private Instant timestamp;
     private String code;
     private String message;
+    Map<String, Object> details;
+    String exceptionType; // 발생한 예외의 클래스 이름
+    int status; // HTTP 상태코드
 }
