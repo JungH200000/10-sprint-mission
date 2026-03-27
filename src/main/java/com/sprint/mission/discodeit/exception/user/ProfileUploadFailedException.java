aa0a7abb6d22da478aa42765a269bdbb118e31bd
JungH200000 <jhpaxk0604@gmail.com>
2026-03-27 15:19:49 +0900

    refactor: 실수로 다른 예외 클래스를 상속하도록 작성한 것을 `UserException`을 상속하도록 수정

--- src/main/java/com/sprint/mission/discodeit/exception/user/ProfileUploadFailedException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/user/ProfileUploadFailedException.java
@@ -1,12 +1,11 @@
 package com.sprint.mission.discodeit.exception.user;
 
-import com.sprint.mission.discodeit.exception.DiscodeitException;
 import com.sprint.mission.discodeit.exception.ErrorCode;
 
 import java.util.Map;
 import java.util.UUID;
 
-public class ProfileUploadFailedException extends DiscodeitException {
+public class ProfileUploadFailedException extends UserException {
 
     public ProfileUploadFailedException(UUID userId, Throwable e) {
         super(ErrorCode.PROFILE_UPLOAD_FAILED, "userId", userId, e);
