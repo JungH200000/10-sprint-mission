eeae7e599ddd775577d83bf2be9a9ac07508eb49
JungH200000 <jhpaxk0604@gmail.com>
2026-05-15 12:22:25 +0900

    refactor: openapi 문서 관련 파일 이동

--- src/main/java/com/sprint/mission/discodeit/config/openapi/OpenApiConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/openapi/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.sprint.mission.discodeit.config;
+package com.sprint.mission.discodeit.config.openapi;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
