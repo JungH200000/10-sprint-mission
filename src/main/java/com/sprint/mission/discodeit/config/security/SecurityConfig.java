13a63c2e483b4a8be848f2df71379901fd66634e
JungH200000 <jhpaxk0604@gmail.com>
2026-05-16 18:51:47 +0900

    feat: PasswordEncoder Bean 등록

--- src/main/java/com/sprint/mission/discodeit/config/security/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
@@ -12,6 +14,7 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 public class SecurityConfig {
 
     @Bean
+    // SecurityFilterChain Bean 등록
     // HttpSecurity를 통해 HTTP 요청에 대한 보안 설정 구성
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -19,8 +22,21 @@ public class SecurityConfig {
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                         .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()));
 
-        log.debug("========== [Spring Security Filter List] ==========");
+        SecurityFilterChain chain = http.build();
 
-        return http.build();
+        log.debug("========== [Spring Security Filter List - START] ==========");
+        chain.getFilters().forEach(filter ->
+                log.debug("{}", filter.getClass().getSimpleName())
+        );
+        log.debug("========== [Spring Security Filter List - END] ==========");
+
+        return chain;
+    }
+
+    @Bean
+    // PasswordEncoder Bean 등록
+    // 비밀번호를 bcrypt 알고리즘으로 해시 처리 - 같은 비밀번호라도 다른 해시값 생성
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
