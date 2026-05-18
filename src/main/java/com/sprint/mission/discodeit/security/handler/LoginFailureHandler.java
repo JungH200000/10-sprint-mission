1c5086b8af702a3f7b945eb8c240fbc97e775879
JungH200000 <jhpaxk0604@gmail.com>
2026-05-18 11:52:37 +0900

    feat: 로그인 인증 실패 후 실행되는 핸들러 클래스 구현

---
+++ src/main/java/com/sprint/mission/discodeit/security/handler/LoginFailureHandler.java
@@ -0,0 +1,47 @@
+package com.sprint.mission.discodeit.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.exception.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+// 로그인 인증 실패 후 실행되는 핸들러 클래스
+public class LoginFailureHandler implements AuthenticationFailureHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationFailure(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException exception
+    ) throws IOException, ServletException
+    {
+        // 예외 응답 생성
+        ErrorResponse errorResponse = ErrorResponse.authenticationFailure(exception, 401);
+
+        // response
+        // 응답 상태 코드 401
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        // 응답 body가 JSON
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        // 한글 깨짐 방지를 위해 UTF-8 인코딩 설정
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        // ErrorResponse 객체 -> JSON으로 변환 후 응답 Body에 담음
+        objectMapper.writeValue(response.getWriter(), errorResponse);
+    }
+}
