25d4c89673fc98042304db7edcbf0b999724af3b
JungH200000 <jhpaxk0604@gmail.com>
2026-05-18 15:53:45 +0900

    feat: `SecurityFilterChain`에 로그아웃 설정 추가

--- src/main/java/com/sprint/mission/discodeit/config/security/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/security/SecurityConfig.java
@@ -6,10 +6,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -32,7 +34,11 @@ public class SecurityConfig {
                         .loginProcessingUrl("/api/auth/login")
                         .successHandler(loginSuccessHandler)
                         .failureHandler(loginFailureHandler))
-                ;
+                .logout(logout -> logout
+                        .logoutUrl("/api/auth/logout")
+                        .logoutSuccessHandler(
+                                new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT)))
+        ;
 
         SecurityFilterChain chain = http.build();
 
