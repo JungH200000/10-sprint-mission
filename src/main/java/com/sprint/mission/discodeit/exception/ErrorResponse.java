97cf0b2bfb353f5a4b6e8bf9bb75f85667cb4410
JungH200000 <jhpaxk0604@gmail.com>
2026-05-19 12:03:24 +0900

    feat: 인증되지 않거나 권한이 부족할 때 `ErrorResponse`를 생성하는 정적 팩토리 메서드 구형

--- src/main/java/com/sprint/mission/discodeit/exception/ErrorResponse.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ErrorResponse.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 
 import java.time.Instant;
@@ -15,9 +16,9 @@ public class ErrorResponse {
     private Instant timestamp;
     private String code;
     private String message;
-    Map<String, Object> details;
-    String exceptionType; // 발생한 예외의 클래스 이름
-    int status; // HTTP 상태코드
+    private Map<String, Object> details;
+    private String exceptionType; // 발생한 예외의 클래스 이름
+    private int status; // HTTP 상태코드
 
     public ErrorResponse(DiscodeitException e, int status) {
         this.timestamp = e.getTimestamp();
@@ -38,11 +39,35 @@ public class ErrorResponse {
     }
 
     // 로그인 실패 전용 정적 팩토리 메서드
-    public static ErrorResponse authenticationFailure(AuthenticationException e, int status) {
+    public static ErrorResponse loginFailure(AuthenticationException e, int status) {
         return new ErrorResponse(
                 Instant.now(),
                 "LOGIN_FAILED",
                 "아이디 또는 비밀번호가 올바르지 않습니다.",
+                new HashMap<>(),
+                e.getClass().getSimpleName(),
+                status
+        );
+    }
+
+    // 인증이 필요한 API에 인증되지 않은 사용자의 접근 차단 전용 정적 팩토리 메서드
+    public static ErrorResponse unauthenticated(AuthenticationException e, int status) {
+        return new ErrorResponse(
+                Instant.now(),
+                "AUTH_UNAUTHENTICATED",
+                "인증되지 않은 사용자는 접근할 수 없습니다.",
+                new HashMap<>(),
+                e.getClass().getSimpleName(),
+                status
+        );
+    }
+
+    // 권한이 부족한 사용자의 접근 차단 전용 정적 팩토리 메서드
+    public static ErrorResponse accessDenied(AccessDeniedException e, int status) {
+        return new ErrorResponse(
+                Instant.now(),
+                "AUTH_ACCESS_DENIED",
+                "접근 권한이 없습니다.",
                 new HashMap<>(),
                 e.getClass().getSimpleName(),
                 status
