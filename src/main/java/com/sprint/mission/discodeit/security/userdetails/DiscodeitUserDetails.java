b4b9b0c2e547eff32953212a91ef7ac590d41e19
JungH200000 <jhpaxk0604@gmail.com>
2026-05-19 17:34:49 +0900

    feat: 사용자 계정 비교 및 해시값 반환 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/security/userdetails/DiscodeitUserDetails.java
+++ src/main/java/com/sprint/mission/discodeit/security/userdetails/DiscodeitUserDetails.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 // Spring Security의 formLogin 인증 과정에서 사용되는 사용자 정보 객체
 // 사용자가 인증에 성공 시, 이 객체가 Authentication의 principal로 저장됨
@@ -57,5 +58,25 @@ public class DiscodeitUserDetails implements UserDetails {
     // 계정 활성화 여부
     public boolean isEnabled() {
         return true;
+    }
+
+    // 사용자 식별값(`id`)을 기준으로 해시값 반환
+    // SessionRegister 내부 Map이 principal 객체를 빠르게 찾을 때 사용
+    @Override
+    public int hashCode() {
+        return this.userDto.id().hashCode();
+    }
+
+    // 같은 사용자 계정인지 비교
+    // Spring Security의 SessionRegistry가 동일 계정의 세션을 구분할 때 사용
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof DiscodeitUserDetails)) {
+            return false;
+        }
+        return this.userDto.id().equals(((DiscodeitUserDetails) obj).getUserDto().id());
     }
 }
