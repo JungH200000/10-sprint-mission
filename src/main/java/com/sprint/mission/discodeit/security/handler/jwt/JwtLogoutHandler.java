8ac0e55c4fa4ec702db6b85fcbac3cadee7f4651
JungH200000 <jhpaxk0604@gmail.com>
2026-05-25 20:04:38 +0900

    feat: Jwt 기반 로그아웃 handler 구현

---
+++ src/main/java/com/sprint/mission/discodeit/security/handler/jwt/JwtLogoutHandler.java
@@ -0,0 +1,40 @@
+package com.sprint.mission.discodeit.security.handler.jwt;
+
+import com.sprint.mission.discodeit.dto.user.UserDto;
+import com.sprint.mission.discodeit.security.jwt.JwtTokenProvider;
+import com.sprint.mission.discodeit.security.userdetails.DiscodeitUserDetails;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class JwtLogoutHandler implements LogoutHandler {
+
+    @Override
+    public void logout(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) {
+        ResponseCookie logoutRefreshTokenCookie = ResponseCookie
+                .from("REFRESH_TOKEN", "")
+                .httpOnly(true)
+                .secure(false) // local용
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, logoutRefreshTokenCookie.toString());
+
+        log.info("[AUTH_LOGOUT_SUCCESS] 로그아웃 성공");
+    }
+}
