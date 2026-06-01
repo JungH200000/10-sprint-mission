ea6ecb5526107f2723d0744d5da39afd72d0abfa
JungH200000 <jhpaxk0604@gmail.com>
2026-06-01 17:17:22 +0900

    feat: 커스텀 예외 코드 추가

--- src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
@@ -43,6 +43,8 @@ public enum ErrorCode {
     INVALID_JWT_TOKEN("유효하지 않은 Token입니다."),
     INVALID_ACCESS_TOKEN("유효하지 않은 Access Token입니다."),
     INVALID_REFRESH_TOKEN("유효하지 않은 Refresh Token입니다."),
+    INVALID_JWT_INFORMATION("유효하지 않은 Jwt Information입니다."),
+    JWT_INFORMATION_NOT_FOUND("Jwt Information을 찾을 수 없습니다."),
 
     // Server
     INTERNAL_SERVER_ERROR("서버에 오류가 발생했습니다."); // 커스텀 예외로 처리하지 못하는 예외 코드
