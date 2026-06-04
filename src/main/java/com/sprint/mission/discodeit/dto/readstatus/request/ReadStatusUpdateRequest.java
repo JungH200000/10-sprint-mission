75f4c1ad77ff8a600d3398e723ba82f16643353b
JungH200000 <jhpaxk0604@gmail.com>
2026-06-05 01:08:10 +0900

    feat: `ReadStatusUpdateRequest`에 `newNotificationEnabled` 필드 추가

--- src/main/java/com/sprint/mission/discodeit/dto/readstatus/request/ReadStatusUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/readstatus/request/ReadStatusUpdateRequest.java
@@ -8,6 +8,9 @@ import java.time.Instant;
 @Schema(description = "수정할 읽음 상태 정보")
 public record ReadStatusUpdateRequest(
         @NotNull(message = "newLastReadAt이 null로 입력되었습니다.")
-        Instant newLastReadAt
+        Instant newLastReadAt,
+
+        @NotNull(message = "알림 수정 여부가 null로 입력되었습니다.")
+        boolean newNotificationEnabled
 ) {
 }
