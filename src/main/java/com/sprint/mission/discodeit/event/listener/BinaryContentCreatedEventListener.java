02a4ae587ea5370bd840bc4cb3fa767311194b2f
JungH200000 <jhpaxk0604@gmail.com>
2026-06-04 17:32:12 +0900

    refactor: `try...catch`문으로 `BinaryContent` status 업데이트 분기 로직 리팩터링

--- src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentCreatedEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentCreatedEventListener.java
@@ -1,11 +1,8 @@
 package com.sprint.mission.discodeit.event.listener;
 
-import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.BinaryContentStatus;
 import com.sprint.mission.discodeit.event.BinaryContentCreatedEvent;
-import com.sprint.mission.discodeit.exception.binarycontent.BinaryContentNotFoundException;
-import com.sprint.mission.discodeit.exception.common.InvalidInputException;
-import com.sprint.mission.discodeit.repository.BinaryContentRepository;
+import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +20,7 @@ public class BinaryContentCreatedEventListener {
 
     // Binary 파일을 저장하는 저장소
     private final BinaryContentStorage binaryContentStorage;
-    private final BinaryContentRepository binaryContentRepository;
+    private final BinaryContentService binaryContentService;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleBinaryContentCreatedListener(
@@ -32,22 +29,22 @@ public class BinaryContentCreatedEventListener {
         UUID binaryContentId = event.getBinaryContentId();
         byte[] bytes = event.getBytes();
 
-        // Binary 데이터를 저장소에 저장
-        binaryContentStorage.put(binaryContentId, bytes);
+        try {
+            // Binary 데이터를 저장소에 저장
+            binaryContentStorage.put(binaryContentId, bytes);
 
-        // BinaryContent status를 SUCCESS로 업데이트
-        BinaryContent binaryContent = validateAndGetBinaryContentByBinaryContentId(binaryContentId);
-        binaryContent.updateStatus(BinaryContentStatus.SUCCESS);
+        } catch (Exception e) {
+            // 저장(put) 실패 시
+            binaryContentService.updateStatus(binaryContentId, BinaryContentStatus.FAIL);
 
-        log.debug("[BINARY_CONTENT_UPLOAD] Binary 파일 저장 완료: binaryContentId={}, status={}, size={}",
-                binaryContentId, binaryContent.getStatus().toString(), bytes.length);
-    }
-
-    private BinaryContent validateAndGetBinaryContentByBinaryContentId(UUID binaryContentId) {
-        if (binaryContentId == null) {
-            throw new InvalidInputException("binaryContentId", null);
+            log.error("[BINARY_CONTENT_UPLOAD_FAIL] Binary 파일 저장 실패: binaryContentId={}, status={}, size={}",
+                    binaryContentId, BinaryContentStatus.FAIL, bytes.length, e);
         }
-        return binaryContentRepository.findById(binaryContentId)
-                .orElseThrow(() -> new BinaryContentNotFoundException(binaryContentId));
+
+        // 저장(put) 성공 시 BinaryContent status를 SUCCESS로 업데이트
+        binaryContentService.updateStatus(binaryContentId, BinaryContentStatus.SUCCESS);
+
+        log.debug("[BINARY_CONTENT_UPLOAD_SUCCESS] Binary 파일 저장 완료: binaryContentId={}, status={}, size={}",
+                binaryContentId, BinaryContentStatus.SUCCESS, bytes.length);
     }
 }
