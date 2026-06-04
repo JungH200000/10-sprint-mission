09b122798b75977763b53e1858b41830af1db24b
JungH200000 <jhpaxk0604@gmail.com>
2026-06-05 01:35:11 +0900

    feat: 사용자 권한(Role) 변경 시 알림 발생을 요청하는 이벤트 클래스 구현

---
+++ src/main/java/com/sprint/mission/discodeit/event/RoleUpdatedEvent.java
@@ -0,0 +1,32 @@
+package com.sprint.mission.discodeit.event;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.exception.common.InvalidInputException;
+import lombok.Getter;
+
+import java.time.Instant;
+
+// 권한 변경시 알림 발생을 요청하는 이벤트 클래스
+@Getter
+public class RoleUpdatedEvent {
+
+    private final User user;
+
+    // 이벤트 생성 시간
+    private final Instant occurredAt;
+
+
+    // 이벤트 생성자
+    public RoleUpdatedEvent(
+            User user
+    ) {
+        if (user == null) {
+            throw new InvalidInputException("user", null);
+        }
+
+        this.user = user;
+        this.occurredAt = Instant.now();
+    }
+
+
+}
