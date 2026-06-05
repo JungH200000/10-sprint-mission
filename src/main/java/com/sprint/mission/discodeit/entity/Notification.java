5096cac742e69a29cd5be0aad2d3afe55ce9d51f
JungH200000 <jhpaxk0604@gmail.com>
2026-06-06 00:14:29 +0900

    fix: 메시지 `content`가 `null`도 허용하도록 수정

--- src/main/java/com/sprint/mission/discodeit/entity/Notification.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Notification.java
@@ -18,7 +18,7 @@ public class Notification extends BaseUpdatableEntity {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column
     private String content;
 
     public Notification(
