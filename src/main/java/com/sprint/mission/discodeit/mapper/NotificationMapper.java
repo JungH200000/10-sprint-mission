100b1ffe0609192048f1db1dde95e4c494ff972f
JungH200000 <jhpaxk0604@gmail.com>
2026-06-05 13:53:07 +0900

    fix: `NotificationMapper`에서 `source` 설정 오류 수정

--- src/main/java/com/sprint/mission/discodeit/mapper/NotificationMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/NotificationMapper.java
@@ -3,11 +3,9 @@ package com.sprint.mission.discodeit.mapper;
 import com.sprint.mission.discodeit.dto.notification.NotificationDto;
 import com.sprint.mission.discodeit.entity.Notification;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {UserMapper.class})
 public abstract class NotificationMapper {
 
-    @Mapping(source = "user.id", target = "receiverId")
     public abstract NotificationDto toDto(Notification notification);
 }
