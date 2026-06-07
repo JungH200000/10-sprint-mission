a41a924894ba4717a65f4e3ccae6f271995691bc
JungH200000 <jhpaxk0604@gmail.com>
2026-06-07 15:20:11 +0900

    refactor: `message` 객체 대신 값을 event 객체로 넘김

--- src/main/java/com/sprint/mission/discodeit/event/MessageCreatedEvent.java
+++ src/main/java/com/sprint/mission/discodeit/event/MessageCreatedEvent.java
@@ -1,29 +1,43 @@
 package com.sprint.mission.discodeit.event;
 
-import com.sprint.mission.discodeit.entity.Message;
-import com.sprint.mission.discodeit.exception.common.InvalidInputException;
+import com.sprint.mission.discodeit.entity.ChannelType;
 import lombok.Getter;
 
 import java.time.Instant;
+import java.util.UUID;
 
 // 채널에 새로운 메시지가 생성될 시 알림 발생을 요청하는 이벤트 클래스
 @Getter
 public class MessageCreatedEvent {
 
-    private final Message message;
+    private final String messageContent;
+
+    private final UUID channelId;
+    private final ChannelType channelType;
+    private final String channelName;
+
+    private final UUID authorId;
+    private final String authorName;
 
     // 이벤트 생성 시간
     private final Instant occurredAt;
 
     // 이벤트 생성자
     public MessageCreatedEvent(
-            Message message
+            String messageContent,
+            UUID channelId,
+            ChannelType channelType,
+            String channelName,
+            UUID authorId,
+            String authorName
     ) {
-        if (message == null) {
-            throw new InvalidInputException("message", null);
-        }
+        this.messageContent = messageContent;
+        this.channelId = channelId;
+        this.channelType = channelType;
+        this.channelName = channelName;
+        this.authorId = authorId;
+        this.authorName = authorName;
 
-        this.message = message;
         this.occurredAt = Instant.now();
     }
 }
