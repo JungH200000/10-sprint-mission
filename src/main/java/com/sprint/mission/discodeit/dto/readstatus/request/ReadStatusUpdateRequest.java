845f9d13c714ef5356d532e217819da6b4035ee7
JungH200000 <jhpaxk0604@gmail.com>
2026-06-07 18:40:02 +0900

    fix: `newLastReadAt`이 `null`로 입력되므로 `@NotNull` Bean Validation 제거

--- src/main/java/com/sprint/mission/discodeit/dto/readstatus/request/ReadStatusUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/readstatus/request/ReadStatusUpdateRequest.java
@@ -7,7 +7,6 @@ import java.time.Instant;
 
 @Schema(description = "수정할 읽음 상태 정보")
 public record ReadStatusUpdateRequest(
-        @NotNull(message = "newLastReadAt이 null로 입력되었습니다.")
         Instant newLastReadAt,
 
         Boolean newNotificationEnabled
