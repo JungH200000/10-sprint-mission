06e9af5e74aea0b1541892b792d982c81259fc5f
JungH200000 <jhpaxk0604@gmail.com>
2026-06-07 21:41:53 +0900

    feat: `Caffeine` 캐시 설정 클래스 구현

---
+++ src/main/java/com/sprint/mission/discodeit/config/cache/CacheConfig.java
@@ -0,0 +1,10 @@
+package com.sprint.mission.discodeit.config.cache;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+// 캐시 환경을 설정하는 클래스
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
